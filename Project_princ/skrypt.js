9941703701842f447ee6beed85c09fe8fd3c088b
SviatoslavZinch <89101855+SviatoslavZinch@users.noreply.github.com>
2024-05-13 12:07:08 +0200

    Delete Project_princ directory

--- Project_princ/skrypt.js
+++
@@ -1,9 +0,0 @@
-function wyk(){
-    let id1=document.getElementById("id1").value;
-    let id2=document.getElementById("id2").value;
-    let id3=document.getElementById("id3").value;
-    let id4=document.getElementById("id4").value;
-    
-
-
-}
