65b37fc3ef935db8c091b6cd56c854384706fe8f
SviatoslavZinch <89101855+SviatoslavZinch@users.noreply.github.com>
2024-05-13 12:07:33 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,9 @@
+function wyk(){
+    let id1=document.getElementById("id1").value;
+    let id2=document.getElementById("id2").value;
+    let id3=document.getElementById("id3").value;
+    let id4=document.getElementById("id4").value;
+    
+
+
+}
